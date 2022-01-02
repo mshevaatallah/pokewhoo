20ea369cab29765d3f76a0ad8256f1d794addec5
sxhvxa <muhammadshax07@gmail.com>
2022-01-02 18:33:38 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [nama, setNama] = useState();
+  const [hasil, setHasil] = useState(null);
+
+  function typing(e) {
+    setNama(e.target.value);
+  }
+
+  function click() {
+    async function getData() {
+      const res = await axios.get('https://pokeapi.co/api/v2/pokemon/' + nama);
+      setHasil(res.data);
+    }
+    getData();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src="pokemon.png" width="100" alt="" />
+      <h1 className="title">PokeWho?</h1>
+      <p>Find your Pokemon Now !</p>
+      <div className="form">
+        <input type="text" placeholder="Enter your Pokemon's name !" onChange={typing} />
+        <button onClick={click}> Find </button>
+      </div>
+      {hasil && (
+        <div className="content">
+          <h2>Your Pokemon result :</h2>
+
+          <img src={hasil.sprites.other['official-artwork'].front_default} alt="pokemonsimg" width="100" />
+          <h3>Name: {hasil.name} </h3>
+          <p>ID: {hasil.id} </p>
+          <p>Weight: {hasil.weight}</p>
+        </div>
+      )}
+      <footer>
+        <i>created by shev &copy;</i>
+      </footer>
     </div>
   );
 }
